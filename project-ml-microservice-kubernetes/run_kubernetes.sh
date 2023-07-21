1c2d03b1e7bbf6c0fd180040070a3dd92a64c69d
duyhieu108 <duyhieu1008@gmail.com>
2023-07-21 22:58:30 +0800

    Update run_kubernetes.sh

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+docker_path=hieudd
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy project4 --image="$docker_path/pyapp:project4"
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment.apps/project4 8000:80
